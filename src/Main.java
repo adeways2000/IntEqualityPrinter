db37fd7570b09b659deccecaa500c1f913450a47
ayokunle adeleye <adeways2000@gmail.com>
2021-04-30 11:38:25 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+
+    public static void main(String[] args) {
+
+        IntEqualityPrinter.printEqual(7,7,7);
+        IntEqualityPrinter.printEqual(1 ,1 ,2);
+        IntEqualityPrinter.printEqual(-24 ,-25 ,-12);
+        IntEqualityPrinter.printEqual(24 ,25 ,12);
+    }
+}
